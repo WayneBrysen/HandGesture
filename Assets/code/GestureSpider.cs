474738b0088a4acc981ce89a429420be28fb1ffc
WayneBrysen <q1092727520@gmail.com>
2024-09-18 15:03:23 +1000

    scene fixed

--- Assets/code/GestureSpider.cs
+++ Assets/code/GestureSpider.cs
@@ -1,63 +1,144 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class GestureSpider : MonoBehaviour
 {
-    public GameObject markerPrefab;
-    public Transform handTransform;
+    public GameObject arrowPrefab;
+    public Transform cameraTransform;
     public LayerMask raycastLayerMask;
+    public float arrowDistance = 20f;
+    public float arrowBufferDistance = 0.5f;
+    public GameObject xrOrigin;
+    public float arrowYPosition = 1.0f;
+    public Transform handTransform;
+    private GameObject currentArrow;
+    private Coroutine gestureCoroutine;
     private bool isGestureActive = false;
-    private GameObject currentMarker;
+    private bool isRotatingGestureActive = false;
+    private float gestureMaintainTimer = 0f;
+    private float gestureDurationThreshold = 2f;
+    private Vector3 initialHandPosition;
+    public float rotationSensitivity = 1000f; 
 
     void Update()
     {
-        if (isGestureActive)
+        if (!isGestureActive)
         {
-            UpdateMarkerPosition();
+            gestureMaintainTimer += Time.deltaTime;
+
+            if (gestureMaintainTimer >= gestureDurationThreshold)
+            {
+                RemoveArrow();
+            }
         }
-        else
+
+        if (isRotatingGestureActive && currentArrow != null)
         {
-            RemoveMarker();  
+            UpdateArrowRotationBasedOnHandMovement();
         }
     }
 
-    private void UpdateMarkerPosition()
+    private void InstantiateArrow()
     {
-        Ray ray = new Ray(handTransform.position, handTransform.forward);
+        Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, 100f, raycastLayerMask))  
+        Vector3 spawnPosition;
+
+        if (Physics.Raycast(ray, out hit, 100f, raycastLayerMask))
         {
-            if (currentMarker == null)
-            {
-                currentMarker = Instantiate(markerPrefab, hit.point, Quaternion.identity);
-            }
-            else 
-            {
-                currentMarker.transform.position = hit.point;
-            }
+            spawnPosition = hit.point - ray.direction * arrowBufferDistance;
         }
+        else
+        {
+            spawnPosition = cameraTransform.position + cameraTransform.forward * arrowDistance;
+        }
+
+        spawnPosition.y = arrowYPosition;
+
+        currentArrow = Instantiate(arrowPrefab, spawnPosition, Quaternion.LookRotation(new Vector3(cameraTransform.forward.x, 0, cameraTransform.forward.z)));
     }
 
     public void GestureActive()
     {
+        if (currentArrow == null)
+        {
+            InstantiateArrow();
+        }
+
         isGestureActive = true;
+        gestureMaintainTimer = 0f;
     }
 
     public void GestureEnd()
     {
         isGestureActive = false;
-
+        gestureMaintainTimer = 0f;
     }
 
-    private void RemoveMarker()
+    public void StartRotatingGesture()
     {
-        if (currentMarker != null)
+        isRotatingGestureActive = true;
+        initialHandPosition = handTransform.position;
+    }
+
+    public void EndRotatingGesture()
+    {
+        isRotatingGestureActive = false;
+        if (currentArrow != null)
         {
-            Destroy(currentMarker);
+            StartCoroutine(SmoothMoveToArrow());
         }
     }
 
+    private void RemoveArrow()
+    {
+        if (currentArrow != null)
+        {
+            Destroy(currentArrow);
+            currentArrow = null;
+        }
+    }
 
+    private IEnumerator SmoothMoveToArrow()
+    {
+        float duration = 1.0f;
+        float elapsed = 0f;
+        Vector3 startPosition = xrOrigin.transform.position;
+        Quaternion startRotation = xrOrigin.transform.rotation;
+        Vector3 targetPosition = new Vector3(
+        currentArrow.transform.position.x,
+        xrOrigin.transform.position.y,
+        currentArrow.transform.position.z);
+        Quaternion targetRotation = currentArrow.transform.rotation;
+
+        while (elapsed < duration)
+        {
+
+            xrOrigin.transform.position = Vector3.Lerp(startPosition, targetPosition, elapsed / duration);
+            xrOrigin.transform.rotation = Quaternion.Slerp(startRotation, targetRotation, elapsed / duration);
+
+            elapsed += Time.deltaTime;
+            yield return null; 
+        }
+
+        xrOrigin.transform.position = targetPosition;
+        xrOrigin.transform.rotation = targetRotation;
+
+        RemoveArrow();
+    }
+
+    private void UpdateArrowRotationBasedOnHandMovement()
+    {
+        if (currentArrow == null) return;
+
+        float handMovementDelta = handTransform.position.x - initialHandPosition.x;
+
+        currentArrow.transform.Rotate(Vector3.up, handMovementDelta * rotationSensitivity);
+
+        initialHandPosition = handTransform.position;
+    }
 }
+
