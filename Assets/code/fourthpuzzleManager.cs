d38b638ce4108bdb28c2a4d6441a10fc5614a882
WayneBrysen <q1092727520@gmail.com>
2024-10-02 08:56:27 +1000

    finish!

---
+++ Assets/code/fourthpuzzleManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fourthpuzzleManager : MonoBehaviour
+{
+    // Variables for books
+    public Transform playerTransform;
+    public GameObject targetPosition;
+    public BookHolder[] placeHolders;
+
+    // Variables for buttons
+    public GameObject[] buttons;
+    private int buttonsPressed = 0;
+
+    // Check if all books are collected
+    private bool AllBooksCollected()
+    {
+        foreach (BookHolder bookHolder in placeHolders)
+        {
+            if (!bookHolder.IsFilled())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Check if all buttons are pressed
+    private bool AllButtonsPressed()
+    {
+        return buttonsPressed == buttons.Length;
+    }
+
+    // Call this method when a button is pressed
+    public void OnButtonPressed()
+    {
+        buttonsPressed++;
+        CheckPuzzleCompletion();
+    }
+
+    // Check both conditions
+    public void CheckPuzzleCompletion()
+    {
+        if (AllBooksCollected() && AllButtonsPressed())
+        {
+            TeleportPlayer();
+        }
+    }
+
+    // Method to teleport the player
+    private void TeleportPlayer()
+    {
+        Vector3 newPosition = new Vector3(targetPosition.transform.position.x,
+                                          playerTransform.position.y,
+                                          targetPosition.transform.position.z);
+        playerTransform.position = newPosition;
+        Debug.Log("Player has been teleported to the target position!");
+    }
+}
